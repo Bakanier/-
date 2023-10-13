a5a6dea88783102641849af9a193bb6868fdc486
Bakanier <kushnazaruul@gmail.com>
2023-10-13 13:33:02 +0600

    Переменные и типы данных

---
+++ main.js
@@ -0,0 +1,22 @@
+const First_Name = "Бекназар";
+const Age ="21";
+const City = "Бишкек";
+
+console.log("Имя:", First_Name );
+console.log("Возраст:", Age);
+console.log("Город:", City);
+
+const favorite_number = 8; 
+const favorite_color = "синий";
+
+
+console.log("Тип данных переменной любимое_число:", favorite_number, typeof favorite_number);
+console.log("Тип данных переменной любимый_цвет:", favorite_color, typeof favorite_color);
+
+
+const первое_слово = "Hello";
+const второе_слово = "world";
+
+const объединенная_строка = первое_слово + " " + второе_слово;
+
+console.log(объединенная_строка);
